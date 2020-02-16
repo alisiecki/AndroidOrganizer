377e2b02c6d849e8ce6fcd778226a55931535459
szerholder <lisadesh1@gmail.com>
2020-02-16 16:22:38 +0100

    build folder added

---
+++ app/build/generated/source/buildConfig/debug/com/ad44/organizer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ad44.organizer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ad44.organizer";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
